34f4eb3ddae9afcfa36622e1799362f651073e5c
Bartek <supereddek13@gmail.com>
2023-03-29 02:52:22 +0200

    Działa więcej piłek, zniakają po 6 odbiciach

--- Main.cpp
+++ Main.cpp
@@ -5,11 +5,11 @@
 #include <random>
 #include <GLFW/glfw3.h>
 
-const float M_PI = 3.14;
+float M_PI = 3.14f;
 // Define ball class
 class Ball {
 public:
-    Ball(float x, float y, float radius, float dx, float dy, float r, float g, float b) : x(x), y(y), radius(radius), dx(dx), dy(dy), r(r), g(g), b(b) {}
+    Ball(float x, float y, float radius, float dx, float dy, float r, float g, float b) : x(x), y(y), radius(radius), dx(dx), dy(dy), r(r), g(g), b(b), bounces(0) {}
 
     void draw() {
         glBegin(GL_TRIANGLE_FAN);
@@ -24,11 +24,16 @@ public:
     void move() {
         x += dx;
         y += dy;
-        if (x < -1 + radius || x > 1 - radius) dx = -dx;
-        if (y < -1 + radius || y > 1 - radius) dy = -dy;
+        if (x < -1 + radius || x > 1 - radius) { dx = -dx; bounces++; }
+        if (y < -1 + radius || y > 1 - radius) { dy = -dy; bounces++; }
+    }
+
+    bool shouldRemove() {
+        return bounces >= 6;
     }
 
     float x, y, radius, dx, dy, r, g, b;
+    int bounces;
 };
 
 // Define function to spawn new balls
@@ -38,7 +43,7 @@ void spawnBalls(std::vector<Ball>& balls, int& ballCount) {
     std::uniform_real_distribution<float> posDist(0.05f, 0.95f);
     std::uniform_real_distribution<float> dirDist(-0.02f, 0.02f);
     std::uniform_real_distribution<float> colorDist(0.0f, 1.0f);
-    while (ballCount < 10) {
+    while (ballCount < 10000) {
         float x = posDist(gen);
         float y = posDist(gen);
         float dx = dirDist(gen);
@@ -81,57 +86,36 @@ int main() {
     std::thread ballThread(spawnBalls, std::ref(balls), std::ref(ballCount));
 
     // Main loop
-    /*
     while (!glfwWindowShouldClose(window)) {
-        // Clear screen
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw balls
-        for (auto& ball : balls) {
-            ball.draw();
-            ball.move();
-        }
-
-        // Remove oldest ball if count exceeds maximum
-        if (ballCount > 10) {
-            balls.erase(balls.begin());
-            ballCount--;
-        }
-
-        // Swap buffers and poll events
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }  */
-
-    // Main loop
-    while (!glfwWindowShouldClose(window)) {
-        // Set viewport
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
         glViewport(0, 0, width, height);
-
         // Clear screen
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Draw balls
-        for (auto& ball : balls) {
+        for (auto it = balls.begin(); it != balls.end(); ) {
+            auto& ball = *it;
             ball.draw();
             ball.move();
+            if (ball.shouldRemove()) {
+                it = balls.erase(it);
+                ballCount--;
+            }
+            else {
+                ++it;
+            }
         }
 
-        // Remove oldest ball if count exceeds maximum
-        if (ballCount > 10) {
-            balls.erase(balls.begin());
-            ballCount--;
-        }
-
-        // Swap buffers and poll events
+        // Swap buffers
         glfwSwapBuffers(window);
+
+        // Poll for events
         glfwPollEvents();
     }
 
-
-    // Join thread and
+    // Wait for ball thread to finish
     ballThread.join();
 
     // Terminate GLFW
