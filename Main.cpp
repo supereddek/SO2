7cccc4c7da8d5c0deb70a761ab7c2640c61f537c
Bartek <supereddek13@gmail.com>
2023-05-06 16:09:01 +0200

    Works fine with changing sectors

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,14 @@
 #include <GLFW/glfw3.h>
 
 float M_PI = 3.14f;
+int max_balls = 3;
 // Define ball class
 class Ball {
 public:
-    Ball(float x, float y, float radius, float dx, float dy, float r, float g, float b) : x(x), y(y), radius(radius), dx(dx), dy(dy), r(r), g(g), b(b), bounces(0) {}
+    int sector;
+    //Ball(float x, float y, float radius, float dx, float dy, float r, float g, float b) : x(x), y(y), radius(radius), dx(dx), dy(dy), r(r), g(g), b(b), bounces(0) {}
+    Ball(float x, float y, float radius, float dx, float dy, float r, float g, float b, int sector)
+        : x(x), y(y), radius(radius), dx(dx), dy(dy), r(r), g(g), b(b), bounces(0), sector(sector) {}
 
     void draw() {
         glBegin(GL_TRIANGLE_FAN);
@@ -24,8 +28,49 @@ public:
     void move() {
         x += dx;
         y += dy;
-        if (x < -1 + radius || x > 1 - radius) { dx = -dx; bounces++; }
-        if (y < -1 + radius || y > 1 - radius) { dy = -dy; bounces++; }
+
+        int newSector = calculateSector();
+        if (newSector != sector) {
+            sector = newSector;
+            adjustSpeed();
+        }
+
+        if (x < -1 + radius || x > 1 - radius) {
+            dx = -dx;
+            bounces++;
+        }
+        if (y < -1 + radius || y > 1 - radius) {
+            dy = -dy;
+            bounces++;
+        }
+    }
+
+
+    int calculateSector() {
+        // Calculate the sector number based on the ball's x location
+        float sectorWidth = 1.0f / 3.0f;
+        float normalizedX = (x + 1) / 2.0f;
+        return static_cast<int>(normalizedX / sectorWidth);
+    }
+
+    void adjustSpeed() {
+        // Adjust the speed based on the sector number
+        switch (sector) {
+        case 0:
+            dx *= 0.8f;
+            dy *= 0.8f;
+            break;
+        case 1:
+            dx *= 1.2f;
+            dy *= 1.2f;
+            break;
+        case 2:
+            dx *= 0.9f;
+            dy *= 0.9f;
+            break;
+        default:
+            break;
+        }
     }
 
     bool shouldRemove() {
@@ -43,17 +88,37 @@ void spawnBalls(std::vector<Ball>& balls, int& ballCount) {
     std::uniform_real_distribution<float> posDist(0.05f, 0.95f);
     std::uniform_real_distribution<float> dirDist(-0.02f, 0.02f);
     std::uniform_real_distribution<float> colorDist(0.0f, 1.0f);
-    while (ballCount < 10000) {
-        float x = posDist(gen);
-        float y = posDist(gen);
-        float dx = dirDist(gen);
-        float dy = dirDist(gen);
-        float r = colorDist(gen);
-        float g = colorDist(gen);
-        float b = colorDist(gen);
-        balls.emplace_back(x, y, 0.05f, dx, dy, r, g, b);
-        ballCount++;
+
+    while (true) {
+        if (ballCount < max_balls) {
+            // Spawn new ball
+            float x = posDist(gen);
+            float y = posDist(gen);
+            float dx = dirDist(gen);
+            float dy = dirDist(gen);
+            float r = colorDist(gen);
+            float g = colorDist(gen);
+            float b = colorDist(gen);
+            balls.emplace_back(x, y, 0.05f, dx, dy, r, g, b, (ballCount % 3));
+            
+            ballCount++;
+        }
+
+        // Delay for 1 second
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        // Check if any ball should be removed
+        auto it = balls.begin();
+        while (it != balls.end()) {
+            auto& ball = *it;
+            if (ball.shouldRemove()) {
+                it = balls.erase(it);
+                ballCount--;
+            }
+            else {
+                ++it;
+            }
+        }
     }
 }
 
